169e3114eadae96aecb6385193eacd4f60b96481
Schultz2873 <53604561+Schultz2873@users.noreply.github.com>
2026-06-18 23:09:51 -0500

    Update thumbnail and other features

---
+++ data/team-logos.js
@@ -0,0 +1,13 @@
+window.TEAM_LOGOS = {
+  "Gunnarrhea": "images/owners/gunnarrhea.png",
+  "Dixon Cider": "images/owners/dixon-cider.png",
+  "Mactown MacDaddies": "images/owners/mactown-macdaddies.png",
+  "Me So Heorny": "images/owners/me-so-heorny.png",
+  "This is Mizerable": "images/owners/this-is-mizerable.png",
+  "This Is Mizerable": "images/owners/this-is-mizerable.png",
+  "You Don't Know Bo": "images/owners/you-dont-know-bo.png",
+  "Goodyear Gila Monsters": "images/owners/goodyear-gila-monsters.png",
+  "BB's Bold Team": "images/owners/bbs-bold-team.png",
+  "John's Super Team": "images/owners/johns-super-team.png",
+  "BTA Boyz": "images/owners/bta-boyz.png"
+};
